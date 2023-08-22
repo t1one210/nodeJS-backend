948fdb27c065ecbb3452dc466042e71daa14ac29
t1one210 <55877728+t1one210@users.noreply.github.com>
2023-08-22 18:27:21 +0400

    create simple console app

---
+++ notes.controller.js
@@ -0,0 +1,53 @@
+const fs = require('fs/promises')
+const path = require('path')
+const chalk = require('chalk')
+
+const notesPath = path.join(__dirname, 'db.json')
+
+async function addNote(title) {
+
+    const notes = await getNotes()
+
+    const note = {
+        title,
+        id: Date.now().toString()
+    }
+
+    notes.push(note)
+
+    await fs.writeFile(notesPath, JSON.stringify(notes))
+    console.log(chalk.green.bgGreen('Note was added!'))
+}
+
+async function getNotes() {
+    const notes = await fs.readFile(notesPath, {encoding:'utf-8'})
+    return Array.isArray(JSON.parse(notes)) ? JSON.parse((notes)) : []
+}
+
+async function printNotes() {
+    const notes = await getNotes()
+
+    console.log(chalk.bgBlue('Here is the list of notes:'))
+    notes.forEach((note => {
+        console.log(chalk.blue(note.id, note.title))
+    }))
+}
+
+async function removeNote(id) {
+
+    const notes = await getNotes()
+
+    const removeNote = notes.filter((note =>
+        note.id !== id.toString()
+    ))
+
+    console.log(removeNote)
+
+
+    await fs.writeFile(notesPath, JSON.stringify(removeNote))
+    console.log(chalk.green.bgRed('Note was removed!'))
+}
+
+module.exports = {
+    addNote, printNotes, removeNote
+}
